2f73c61498835e66f6e3c1a1d43c119c9d20dec5
apradhn <abhijit.pradhan6@gmail.com>
2015-09-04 14:12:00 -0400

    sets root controller to render index view

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-app.set('views', './views')
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 
 app.get('/', function(req, res) {
-  res.send("Hello, World!");
+  res.render('index', {title: 'Hey', message: 'Hello there!'});
 })
 
 var server = app.listen(3000, function() {
