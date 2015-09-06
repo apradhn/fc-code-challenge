73f4a464fe625271fff79f58e441b3cfe8b99182
apradhn <abhijit.pradhan6@gmail.com>
2015-09-06 18:26:25 -0400

    writes logic to display num readers for stories 2, 4, 5 when mouse leaves on any of them

--- src/public/js/story_states.js
+++ src/public/js/story_states.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.story').mouseenter(function() {
+  $('.story').hover(function() {
     var number = $(this).data('story-number')
     if (number == 2 || number == 3 || number == 4) {
       var two = $('.story[data-story-number=2]');
@@ -14,7 +14,23 @@ $(function() {
       });
 
     }
+  }, function() {
+      var number = $(this).data('story-number')
+      if (number == 2 || number == 3 || number == 4) {
+        var two = $('.story[data-story-number=2]');
+        var three = $('.story[data-story-number=3]');
+        var four = $('.story[data-story-number=4]');
+
+        var stories = [two, three, four];
+
+        stories.forEach(function(story, index){
+          story.children('.description-container').css('display', 'none');
+          story.children('.num-readers-container').css('display', 'blockg');
+        });
+
+      }    
   });
+
 
 
 });
