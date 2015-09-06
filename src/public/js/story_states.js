a22f0d508e47ea9dde5333a42c0abf41c8d2e1e0
apradhn <abhijit.pradhan6@gmail.com>
2015-09-06 18:42:53 -0400

    refactors state change method by using addClass and removeClass in order to keep flexbox css intact

--- src/public/js/story_states.js
+++ src/public/js/story_states.js
@@ -1,53 +1,39 @@
 $(function() {
+  var one = $('.story[data-story-number=1]');
+  var two = $('.story[data-story-number=2]');
+  var three = $('.story[data-story-number=3]');
+  var four = $('.story[data-story-number=4]');
+  var five = $('.story[data-story-number=5]');  
+
   $('.story').hover(function() {
     var number = $(this).data('story-number')
     if (number == 2 || number == 3 || number == 4) {
-      var one = $('.story[data-story-number=1]');
-      var two = $('.story[data-story-number=2]');
-      var three = $('.story[data-story-number=3]');
-      var four = $('.story[data-story-number=4]');
-      var five = $('.story[data-story-number=5]');
-
       var stories = [two, three, four];
-
       stories.forEach(function(story, index){
-        story.children('.description-container').css('display', 'block');
-        story.children('.num-readers-container').css('display', 'none');
+        story.children('.description-container').removeClass('hide');
+        story.children('.num-readers-container').addClass('hide');
       });
-
       stories = [one, five];
-
       stories.forEach(function(story, index) {
-        story.children('.description-container').css('display', 'none');
-        story.children('.num-readers-container').css('display', 'block');
+        story.children('.description-container').addClass('hide');
+        story.children('.num-readers-container').removeClass('hide');
       });
-
     }
-  }, function() {
-      var number = $(this).data('story-number')
-      if (number == 2 || number == 3 || number == 4) {
-        var one = $('.story[data-story-number=1]');
-        var two = $('.story[data-story-number=2]');
-        var three = $('.story[data-story-number=3]');
-        var four = $('.story[data-story-number=4]');
-        var five = $('.story[data-story-number=5]');
-
-        var stories = [two, three, four];
-
-        stories.forEach(function(story, index){
-          story.children('.description-container').css('display', 'none');
-          story.children('.num-readers-container').css('display', 'block');
-        });
-
-        stories = [one, five];
-
-        stories.forEach(function(story, index) {
-          story.children('.description-container').css('display', 'block');
-          story.children('.num-readers-container').css('display', 'none');
-        });
-
-
-      }    
+  }, 
+  function() {
+    var number = $(this).data('story-number')
+    if (number == 2 || number == 3 || number == 4) {
+      var stories = [two, three, four];
+      stories.forEach(function(story, index){
+        story.children('.description-container').addClass('hide');
+        story.children('.num-readers-container').removeClass('hide');
+      });
+      stories = [one, five];
+      stories.forEach(function(story, index) {
+        story.children('.description-container').removeClass('hide');
+        story.children('.num-readers-container').addClass('hide');
+      });
+    }    
   });
 
 
