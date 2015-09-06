7c4a5c86b713030f7a176db1fc13bf9139aa9bec
apradhn <abhijit.pradhan6@gmail.com>
2015-09-06 18:46:39 -0400

    style tweaks

--- src/public/js/story_states.js
+++ src/public/js/story_states.js
@@ -9,12 +9,12 @@ $(function() {
     var number = $(this).data('story-number')
     if (number == 2 || number == 3 || number == 4) {
       var stories = [two, three, four];
-      stories.forEach(function(story, index){
+      stories.forEach(function(story){
         story.children('.description-container').removeClass('hide');
         story.children('.num-readers-container').addClass('hide');
       });
       stories = [one, five];
-      stories.forEach(function(story, index) {
+      stories.forEach(function(story) {
         story.children('.description-container').addClass('hide');
         story.children('.num-readers-container').removeClass('hide');
       });
@@ -24,12 +24,12 @@ $(function() {
     var number = $(this).data('story-number')
     if (number == 2 || number == 3 || number == 4) {
       var stories = [two, three, four];
-      stories.forEach(function(story, index){
+      stories.forEach(function(story){
         story.children('.description-container').addClass('hide');
         story.children('.num-readers-container').removeClass('hide');
       });
       stories = [one, five];
-      stories.forEach(function(story, index) {
+      stories.forEach(function(story) {
         story.children('.description-container').removeClass('hide');
         story.children('.num-readers-container').addClass('hide');
       });
