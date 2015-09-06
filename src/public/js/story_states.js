baaf035a48a48537c37d0cf712310a99966ffdac
apradhn <abhijit.pradhan6@gmail.com>
2015-09-06 18:31:21 -0400

    adds state change to stories 1 and 5 when stories 2, 3, 5 are hovered over

--- src/public/js/story_states.js
+++ src/public/js/story_states.js
@@ -2,9 +2,11 @@ $(function() {
   $('.story').hover(function() {
     var number = $(this).data('story-number')
     if (number == 2 || number == 3 || number == 4) {
+      var one = $('.story[data-story-number=1]');
       var two = $('.story[data-story-number=2]');
       var three = $('.story[data-story-number=3]');
       var four = $('.story[data-story-number=4]');
+      var five = $('.story[data-story-number=5]');
 
       var stories = [two, three, four];
 
@@ -13,20 +15,37 @@ $(function() {
         story.children('.num-readers-container').css('display', 'none');
       });
 
+      stories = [one, five];
+
+      stories.forEach(function(story, index) {
+        story.children('.description-container').css('display', 'none');
+        story.children('.num-readers-container').css('display', 'block');
+      });
+
     }
   }, function() {
       var number = $(this).data('story-number')
       if (number == 2 || number == 3 || number == 4) {
+        var one = $('.story[data-story-number=1]');
         var two = $('.story[data-story-number=2]');
         var three = $('.story[data-story-number=3]');
         var four = $('.story[data-story-number=4]');
+        var five = $('.story[data-story-number=5]');
 
         var stories = [two, three, four];
 
         stories.forEach(function(story, index){
           story.children('.description-container').css('display', 'none');
-          story.children('.num-readers-container').css('display', 'blockg');
+          story.children('.num-readers-container').css('display', 'block');
         });
+
+        stories = [one, five];
+
+        stories.forEach(function(story, index) {
+          story.children('.description-container').css('display', 'block');
+          story.children('.num-readers-container').css('display', 'none');
+        });
+
 
       }    
   });
