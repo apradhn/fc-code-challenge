45a20659cb21b078a3474486d9038efd5ba68e26
apradhn <abhijit.pradhan6@gmail.com>
2015-09-06 18:24:43 -0400

    writes logic to display description content for stories 2, 3, 4 when any of them are hovered on

--- src/public/js/story_states.js
+++ src/public/js/story_states.js
@@ -1,6 +1,20 @@
 $(function() {
-  $('.story').hover(function() {
+  $('.story').mouseenter(function() {
     var number = $(this).data('story-number')
-    console.log(number);
+    if (number == 2 || number == 3 || number == 4) {
+      var two = $('.story[data-story-number=2]');
+      var three = $('.story[data-story-number=3]');
+      var four = $('.story[data-story-number=4]');
+
+      var stories = [two, three, four];
+
+      stories.forEach(function(story, index){
+        story.children('.description-container').css('display', 'block');
+        story.children('.num-readers-container').css('display', 'none');
+      });
+
+    }
   });
+
+
 });
