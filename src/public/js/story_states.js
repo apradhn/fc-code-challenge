54ef996797297fd4576349ccb6fcbf2ea9f7a0d0
apradhn <abhijit.pradhan6@gmail.com>
2015-09-07 13:13:26 -0400

    moves style swapping responsibility to #swapStyles method

--- src/public/js/story_states.js
+++ src/public/js/story_states.js
@@ -5,68 +5,35 @@ $(function() {
   var four = $('.story[data-story-number=4]');
   var five = $('.story[data-story-number=5]');  
   var contentContainer;
+  var noDescription;
+  var hasDescription;
+  var number;
 
   $('.story').hover(function() {
-    var number = $(this).data('story-number');
-    if (number == 2 || number == 3 || number == 4) {
-      var stories = [two, three, four];
-      stories.forEach(function(story){
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').addClass('hide');
-        contentContainer.children('.description-container').removeClass('hide');
-      });
-      stories = [one, five];
-      stories.forEach(function(story) {
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').removeClass('hide');
-        contentContainer.children('.description-container').addClass('hide');
-      });
-    } else {
-      var stories = [one, five];
-      stories.forEach(function(story) {
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').removeClass('hide');
-        contentContainer.children('.description-container').addClass('hide');
-      });
-      stories = [two, three, four];
-      stories.forEach(function(story) {
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').addClass('hide');
-        contentContainer.children('.description-container').removeClass('hide');
-      });
-    }
+    number = $(this).data('story-number');
+    noDescription = [two, three, four];
+    hasDescription = [one, five];
+    swapStyles(noDescription, hasDescription);
   }, 
   function() {
-    var number = $(this).data('story-number')
-    if (number == 2 || number == 3 || number == 4) {
-      var stories = [two, three, four];
-      stories.forEach(function(story){
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').removeClass('hide');
-        contentContainer.children('.description-container').addClass('hide');
-      });
-      stories = [one, five];
-      stories.forEach(function(story) {
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').addClass('hide');
-        contentContainer.children('.description-container').removeClass('hide');
-      });
-    } else {
-      var stories = [one, five];
-      stories.forEach(function(story) {
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').addClass('hide');
-        contentContainer.children('.description-container').removeClass('hide');
-      });
-      stories = [two, three, four];
-      stories.forEach(function(story) {
-        contentContainer = story.children('.content-container');
-        contentContainer.children('.num-readers-container').removeClass('hide');
-        contentContainer.children('.description-container').addClass('hide');
-      });
-    }
+    number = $(this).data('story-number')
+    hasDescription = [two, three, four];
+    noDescription = [one, five];
+    swapStyles(noDescription, hasDescription);
   });
 
-
+  function swapStyles(noDescription, hasDescription) {
+    noDescription.forEach(function(story){
+      contentContainer = story.children('.content-container');
+      contentContainer.children('.num-readers-container').addClass('hide');
+      contentContainer.children('.description-container').removeClass('hide');
+    });
+    hasDescription.forEach(function(story) {
+      contentContainer = story.children('.content-container');
+      contentContainer.children('.num-readers-container').removeClass('hide');
+      contentContainer.children('.description-container').addClass('hide');
+    });
+  }  
 
 });
+
