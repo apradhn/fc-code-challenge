07e9379cca6ff5c59a0be36b4d1dc2a55a561bcb
apradhn <abhijit.pradhan6@gmail.com>
2015-09-07 12:00:12 -0400

    removes story image backgrounds

--- src/public/js/story_states.js
+++ src/public/js/story_states.js
@@ -6,7 +6,8 @@ $(function() {
   var five = $('.story[data-story-number=5]');  
 
   $('.story').hover(function() {
-    var number = $(this).data('story-number')
+    var backgroundCss = $(this).css('background');
+    var number = $(this).data('story-number');
     if (number == 2 || number == 3 || number == 4) {
       var stories = [two, three, four];
       stories.forEach(function(story){
