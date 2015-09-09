8cf4c32b37d4e1328bd8ba62e7af539ee02d5568
apradhn <abhijit.pradhan6@gmail.com>
2015-09-08 21:01:52 -0400

    writes logic for parallax scrolling

--- src/public/js/parallax.js
+++ src/public/js/parallax.js
@@ -1,2 +1,14 @@
 $(function() {
+  $(document).on('scroll', function(event) {
+    var speed = 0.5;
+    var storiesContainer = $('.stories-container');
+    var storiesBackground = $('.stories-background');
+    var titleBackground = $('.title-background');
+    var title = $('.title');
+    var offset = window.pageYOffset;
+    storiesContainer.css('transform', 'translatey(-'+offset+'px)'); 
+    storiesBackground.css('transform', 'translatey(-'+offset * 0.5 +'px)'); 
+    title.css('transform', 'translatey(-'+offset * 0.5 +'px)'); 
+    titleBackground.css('transform', 'translatey(-'+offset * 0.5 +'px)');
+  });
 });
