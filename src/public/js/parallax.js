3590ac75d8fda359afaa08412be610ddc4075487
apradhn <abhijit.pradhan6@gmail.com>
2015-09-08 21:04:52 -0400

    adds parallax scrolling effect to corner ad

--- src/public/js/parallax.js
+++ src/public/js/parallax.js
@@ -5,10 +5,12 @@ $(function() {
     var storiesBackground = $('.stories-background');
     var titleBackground = $('.title-background');
     var title = $('.title');
+    var cornerAdContainer = $('.corner-ad-container');
     var offset = window.pageYOffset;
     storiesContainer.css('transform', 'translatey(-'+offset+'px)'); 
     storiesBackground.css('transform', 'translatey(-'+offset * speed +'px)'); 
     title.css('transform', 'translatey(-'+offset * speed +'px)'); 
     titleBackground.css('transform', 'translatey(-'+offset * speed +'px)');
+    cornerAdContainer.css('transform', 'translatey(-'+offset * speed +'px)');
   });
 });
